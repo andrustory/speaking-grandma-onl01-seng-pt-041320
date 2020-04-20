c875154369566c0c7d1dfccb75256afaa3773677
Andres Carvajal <jc1993box@hotmail.com>
2020-04-20 03:57:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,9 +4,10 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.uppercase
-    ""
+    "NO, NOT SINCE 1938!"
   else 
     "HUH?! SPEAK UP, SONNY!"
+end
   
 speak_to_grandma("I LOVE YOU GRANDMA!")
   
